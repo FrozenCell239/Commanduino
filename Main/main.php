a8a3def3591f51275fdee4d3b47cd7267b6de32b
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2023-02-09 13:38:11 +0100

    Update main.php

--- Main/main.php
+++ Main/main.php
@@ -19,7 +19,7 @@
         ?>
 
         <!--Others.-->
-        <title>Cabinet</title>
+        <title>Tableau de bord</title>
         <link rel="icon" type="image/x-icon" href="images/favicon.png"> <!--Favicon.-->
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!--Permet l'adaptation de la page et la disposition de ses éléments à tous les terminaux.-->
