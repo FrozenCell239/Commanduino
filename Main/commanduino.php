a925cc8ebd56a8b4a7cab9cee5eadb4d296682db
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2023-02-09 17:44:41 +0100

    Add files via upload

---
+++ Main/commanduino.php
@@ -0,0 +1,26 @@
+<?php
+    function init(){
+
+    }
+    function strikeOpen(){
+        $serial = new phpSerial;
+        $serial->deviceSet("COM1");
+        $serial->deviceOpen();
+        $serial->sendMessage('1');
+        $serial->deviceClose();
+        /*
+        if(!$fp = fopen("/dev/serial", "w")){
+            echo("Impossible de communiquer avec la carte Arduino.");
+        }
+        else{
+            fwrite($fp, true);
+            sleep(2);
+            fwrite($fp, false);
+            fclose($fp);
+        };
+        */
+    };
+    function doorOpen(){
+        
+    };
+?>
