8b0e473ce6f6347f5336024bba3dcf4d2aa06148
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2023-02-09 14:19:15 +0100

    Update commanduino.php

--- Main/commanduino.php
+++ Main/commanduino.php
@@ -1,10 +1,14 @@
 <?php
     function strikeOpen(){
-        $fp = fopen("/dev/ttyUSB0", "w");
-        fwrite($fp, true);
-        sleep(2);
-        fwrite($fp, false);
-        fclose($fp);
+        if(!$fp = fopen("/dev/ttyUSB0", "w")){
+            echo("Impossible de communiquer avec la carte Arduino.");
+        }
+        else{
+            fwrite($fp, true);
+            sleep(2);
+            fwrite($fp, false);
+            fclose($fp);
+        };
     };
     function doorOpen(){
         
