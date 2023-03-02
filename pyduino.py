4eb85680a59ae6e4ae8b92e2a73e68ad3e45f6b7
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2023-03-02 09:31:33 +0100

    Add files via upload

---
+++ pyduino.py
@@ -0,0 +1,12 @@
+import serial
+import time
+import sys
+
+portCom = serial.Serial()
+
+portCom.port = "/dev/ttyACM0"
+portCom.baudrate = 115200
+portCom.open()
+if sys.argv == 1 : order = '1';
+if sys.argv == 2 : order = '2';
+portCom.write(order.encode()) 
