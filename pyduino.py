47a47dd5c7f3bdaeebcade91e36f441840ec6ef4
Nyusuka <56803555+Nyusuka@users.noreply.github.com>
2023-03-02 10:26:49 +0100

    Update pyduino.py

--- pyduino.py
+++ pyduino.py
@@ -6,8 +6,8 @@ portCom = serial.Serial();
 portCom.port = "/dev/ttyACM0";
 portCom.baudrate = 115200;
 portCom.open();
-
-if sys.argv != 1 !=2 : error = 'Invalid or absent argument !'; raise Exception(error);
-if sys.argv == 1 : order = '1';
-if sys.argv == 2 : order = '2';
+if sys.argv != '1' != '2' : error = 'Invalid or absent argument !'; raise Exception(error);
+if sys.argv == '1' : order = '1';
+if sys.argv == '2' : order = '2';
 portCom.write(order.encode());
+portCom.close();
