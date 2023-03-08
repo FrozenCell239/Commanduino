1a4b823431ba74fe2eb29f27a718f801b7ae256d
Frozen cell <56803555+Nyusuka@users.noreply.github.com>
2023-03-08 11:39:08 +0100

    Update commanduino.php

--- commanduino.php
+++ commanduino.php
@@ -1,28 +1,30 @@
 <?php
-    /*function strikeOpen(){ //Opens only the strike.
-        $status = 'A';
-        $url = "http://192.168.1.177/?status=".$status; // IP address of the Arduino board
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_exec($ch);
-        curl_close($ch);
+    function strikeOpen(){ //Opens only the strike.
+        $fp = fopen("/dev/ttyACM0", "wb+");
+        if(!$fp){
+            echo "Erreur : impossible de communiquer avec la carte Arduino !";
+        }
+        else{
+            fwrite($fp, '1');
+            fclose($fp);
+        };
     };
     function doorOpen(){ //Opens both the strike and the door.
-        $status = 'B';
-        $url = "http://192.168.1.177/?status=".$status; // IP address of the Arduino board
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_exec($ch);
-        curl_close($ch);
-    };*/
-    function doorControl($ORDER){
+        $fp = fopen("/dev/ttyACM0", "wb+");
+        if(!$fp){
+            echo "Erreur : impossible de communiquer avec la carte Arduino !";
+        }
+        else{
+            fwrite($fp, '2');
+            fclose($fp);
+        };
+    };
+    /*function doorControl($ORDER){
         $url = "http://192.168.1.177/?status=".$ORDER; // IP address of the Arduino board
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_exec($ch);
         curl_close($ch);
-    };
+    };*/
 ?>
